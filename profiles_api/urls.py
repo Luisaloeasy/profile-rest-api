473d333f0ae15a51354399a3be5536b1ac510010
Luis Aloisi <luisaloeasy@gmail.com>
2020-05-21 21:38:22 -0400

    Added POST PUT PATCH and DELETE method

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,6 @@
 
-from django.contrib import admin
 from django.urls import path
+
 from profiles_api import views
 
 urlpatterns = [
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
 
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
-        """Return  listo of APIView features"""
+        """Return  lista of APIView features"""
         an_apiview = [
 
                 'Uses HTTP methods as function ',
@@ -15,3 +20,29 @@ class HelloApiView(APIView):
             
         ]
         return Response({'messages':'Hello!', 'an_apiview':an_apiview}) 
+    
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(                
+                serializer.errors, 
+                status=status.HTTP_400_BAD_REQUEST              
+                 )
+
+    def put(self,request, pk=None):
+        """Update entire object"""
+        return Response({'method':'PUT'})
+
+    def path(self,request, pk=None):
+        """Para hacer updates parciales"""
+        return Response({'method':'PATCH'})
+
+    def delete(self,request, pk=None):
+        """Eliminar objetos"""
+        return Response({'method':'DELETE'})
