aa5cf0337efa77f07f4588662aa0fe16fa6d3deb
Luis Aloisi <luisaloeasy@gmail.com>
2020-05-22 00:56:05 -0400

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
@@ -52,6 +55,7 @@ class HelloApiView(APIView):
         return Response({'method':'DELETE'})
 
 
+
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
 
@@ -95,10 +99,18 @@ class HelloViewSet(viewsets.ViewSet):
         """Delete an object"""
         return Response ({'http_method':'DELETE'})
 
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     """ Handle creating and updating profles """
     serializer_class= serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creatin user auth token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
